5a82f340df28ad9c78474c730a6f6b670860f7cb
pedro31camilo <pedrocamilo536@gmail.com>
2026-05-20 11:53:33 -0300

    Parametrização do chamada de resumo de reviews

--- backend/src/main/java/br/com/TrustReview/service/ProductSummaryAsyncService.java
+++ backend/src/main/java/br/com/TrustReview/service/ProductSummaryAsyncService.java
@@ -6,6 +6,7 @@ import br.com.TrustReview.repository.ProductRepository;
 import br.com.TrustReview.repository.ReviewRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
@@ -21,17 +22,37 @@ public class ProductSummaryAsyncService {
     private final ReviewRepository reviewRepository;
     private final ProductRepository productRepository;
 
+    @Value("${summary.min-reviews}")
+    private int minReviewsToSummarize;
+
     @Async("sentimentAsyncExecutor")
     public void updateProductSummaryAsync(Product product) {
 
-        log.info("🔥 Gerando resumo (ASYNC) para produto id={}", product.getId());
+        log.info("Gerando resumo (ASYNC) para produto id={}", product.getId());
 
         try {
-            // ✅ Recarrega o produto dentro desta transação
             Product managedProduct = productRepository.findById(product.getId())
                 .orElseThrow(() -> new RuntimeException("Produto não encontrado: " + product.getId()));
 
             List<Review> reviews = reviewRepository.findByProductId(managedProduct);
+
+            //Só gera resumo se tiver o mínimo de reviews configurado
+            if (reviews.size() % minReviewsToSummarize != 0) {
+                log.info(
+                    "⏭️ Produto {} não tem novas reviews suficientes para atualização ({}). Resumo não gerado.",
+                    managedProduct.getId(),
+                    minReviewsToSummarize
+                );   
+                  
+                return;
+            } else {
+                log.info(
+                    "Produto {} tem {} reviews. Gerando resumo (mínimo para atualizar: {}).",
+                    managedProduct.getId(),
+                    reviews.size(),
+                    minReviewsToSummarize
+                );
+            }
 
             List<String> pros = reviews.stream()
                 .filter(r -> r.getPros() != null)
@@ -61,7 +82,7 @@ public class ProductSummaryAsyncService {
             managedProduct.setSummary(summary.trim());
             productRepository.save(managedProduct);
 
-            log.info("✅ Resumo do produto {} atualizado.", managedProduct.getId());
+            log.info("Resumo do produto {} atualizado.", managedProduct.getId());
 
         } catch (Exception e) {
             log.error("❌ Erro ao gerar resumo do produto {}.", product.getId(), e);
